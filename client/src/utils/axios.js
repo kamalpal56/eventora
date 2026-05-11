c05f0172499871ec9cc83790bdcdb127eafc55b9
kamalpal56 <145256644+kamalpal56@users.noreply.github.com>
2026-05-12 00:03:17 +0530

    Update axios baseURL to include '/api'

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://eventora-3.onrender.com',
+    baseURL: 'https://eventora-3.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {

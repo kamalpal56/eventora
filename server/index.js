dc304d442faa7509c733aa458a888f070db2eb90
kamal pal <kamalpaul5557@gmail.com>
2026-05-13 00:34:30 +0530

    fix gmail smtp timeout

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGOOSE_URL)
     console.log("error connecting to mongodb:",error)
 });
 
-const PORT=process.env.PORT||5000
+const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
--- server/utils/email.js
+++ server/utils/email.js
@@ -12,14 +12,22 @@ console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
 //     }
 // });
 
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     }
+// });
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 20000,
 });
 
 
